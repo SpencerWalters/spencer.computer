d63ab02ec4385ddc5832d94bfdedafba93e33461
SpencerWalters <spencerjwalters@gmail.com>
2018-01-21 16:03:07 -0600

    Formatted code with 2-space indents instead of 4-space. :)

--- script/index.js
+++ script/index.js
@@ -1,27 +1,27 @@
 var audio_on = 0;
 function play_song() {
-    if (audio_on){
-        document.getElementById("song").muted = false;
-        document.getElementById("song-melody").muted = true;
-    }
+  if (audio_on){
+    document.getElementById("song").muted = false;
+    document.getElementById("song-melody").muted = true;
+  }
 }
 function play_song_melody() {
-    if (audio_on) {
-        document.getElementById("song").muted = true;
-        document.getElementById("song-melody").muted = false;
-    }
+  if (audio_on) {
+    document.getElementById("song").muted = true;
+    document.getElementById("song-melody").muted = false;
+  }
 }
 function audio_toggle() {
-    if (!audio_on){
-        document.getElementById("song").muted = false;
-        document.getElementById("song-melody").muted = true;
-        document.getElementById("audio-button").style.color = "black";
-        audio_on = 1;
-    }
-    else {
-        document.getElementById("song").muted = true;
-        document.getElementById("song-melody").muted = true; 
-        document.getElementById("audio-button").style.color = "#bbb";
-        audio_on = 0;
-    }
+  if (!audio_on){
+    document.getElementById("song").muted = false;
+    document.getElementById("song-melody").muted = true;
+    document.getElementById("audio-button").style.color = "black";
+    audio_on = 1;
+  }
+  else {
+    document.getElementById("song").muted = true;
+    document.getElementById("song-melody").muted = true; 
+    document.getElementById("audio-button").style.color = "#bbb";
+    audio_on = 0;
+  }
 }
